3ca677619741a93c1908057f5765c682751f77c2
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-02-26 13:22:08 +0800

    Hard coded restaurant data in the restaurants component, and passed them to the thumbnails component to render them

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -3,12 +3,51 @@ import NavBar from "./nav";
 import Thumbnail from "./thumbnail";
 
 class Restaurants extends React.Component {
+  state = {
+    restaurants: [
+      {
+        imgUrl: "https://bit.ly/2vx8SGX",
+        name: "Bella Italia",
+        category: ["Italian"],
+        price: 20,
+        likes: 347,
+        time: 15
+      },
+      {
+        imgUrl: "https://bit.ly/2u17mMF",
+        name: "Peloton",
+        category: ["Vegetarian", "Breakfast"],
+        price: 17,
+        likes: 201,
+        time: 20
+      },
+      {
+        imgUrl: "https://bit.ly/2RyB1Ww",
+        name: "Nüde",
+        category: ["Burger", "Vegetarian"],
+        price: 22,
+        likes: 959,
+        time: 12
+      },
+      {
+        imgUrl: "https://bit.ly/2uDvBke",
+        name: "Burgerz",
+        category: ["Burger"],
+        price: 19,
+        likes: 53,
+        time: 35
+      }
+    ]
+  };
+
   render() {
     return (
       <>
         <NavBar />
         <div id="page">
-          <Thumbnail />
+          {this.state.restaurants.map((r, i) => {
+            return <Thumbnail restaurant={r} />;
+          })}
         </div>
       </>
     );
--- src/components/thumbnail.js
+++ src/components/thumbnail.js
@@ -1,26 +1,41 @@
 import React from "react";
 
 class Thumbnail extends React.Component {
+  state = {
+    restaurant: this.props.restaurant
+  };
+  componentWillReceiveProps(nextProp) {
+    this.setState({ restaurant: nextProp.restaurant });
+  }
   render() {
     return (
       <a href="restaurant.html" className="restaurant">
         <div
           className="photo"
-          style={{ backgroundImage: 'url("https://bit.ly/2vx8SGX")' }}
+          style={{ backgroundImage: `url(${this.state.restaurant.imgUrl})` }}
         ></div>
-        <h3>Bella Italia</h3>
+        <h3>{this.state.restaurant.name}</h3>
         <ul className="categories">
-          <li style={{ background: "#DD3C3E" }}>Italian</li>
+          {this.state.restaurant.category.map((e, i) => {
+            return (
+              <li style={{ background: "#DD3C3E" }} key={i}>
+                {e}
+              </li>
+            );
+          })}
         </ul>
         <div className="info">
           <span className="price">
-            <i className="fas fa-dollar-sign"></i>20
+            <i className="fas fa-dollar-sign"></i>
+            {this.state.restaurant.price}
           </span>
           <span className="likes">
-            <i className="fas fa-thumbs-up"></i>347
+            <i className="fas fa-thumbs-up"></i>
+            {this.state.restaurant.likes}
           </span>
           <span className="time">
-            <i className="fas fa-clock"></i>15 min
+            <i className="fas fa-clock"></i>
+            {this.state.restaurant.time} min
           </span>
         </div>
       </a>

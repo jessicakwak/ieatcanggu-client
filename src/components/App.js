7b1d3da730e98e589348aeca8a3c8634906dfb2d
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-02-26 12:35:13 +0800

    set up the router in the App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,16 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "../styles/App.css";
+import Restaurants from "./restaurants";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" component={Restaurants} />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }

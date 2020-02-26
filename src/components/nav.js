39cab81f0177d6c521eb787c409e10ff5276a79d
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-02-26 12:55:46 +0800

    Added a logo to the nav bar. Also defined NavBar as a separate component

---
+++ src/components/nav.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <nav>
+        <div
+          id="logo"
+          style={{ backgroundImage: "url('https://bit.ly/2S2G7JA')" }}
+        ></div>
+        <ul>
+          <li>
+            <a href="/" style={{ borderColor: "#DD3C3E" }}>
+              <span style={{ color: "#DD3C3E" }}>Italian</span>
+            </a>
+          </li>
+          <li>
+            <a href="/" style={{ borderColor: "#3A3335" }}>
+              <span style={{ color: "#3A3335" }}>Burger</span>
+            </a>
+          </li>
+          <li>
+            <a href="/" style={{ borderColor: "#40C9A2" }}>
+              <span style={{ color: "#40C9A2" }}>Vegetarian</span>
+            </a>
+          </li>
+          <li>
+            <a href="/" style={{ borderColor: "#F2B430" }}>
+              <span style={{ color: "#F2B430" }}>Breakfast</span>
+            </a>
+          </li>
+          <li>
+            <a href="/" style={{ borderColor: "#590348" }}>
+              <span style={{ color: "#590348" }}>Japanese</span>
+            </a>
+          </li>
+        </ul>
+        <select>
+          <option value="">Sort by:</option>
+          <option value="">Price</option>
+          <option value="">Delivery Time</option>
+          <option value="">Likes</option>
+        </select>
+        <input type="text" placeholder="Search..." />
+      </nav>
+    );
+  }
+}
+
+export default NavBar;

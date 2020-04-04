1ed76bdfcf541f78171a54e4404517135e10448d
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-04-04 15:14:14 +0800

    current working v

--- src/components/thumbnails.js
+++ src/components/thumbnails.js
@@ -9,16 +9,7 @@ class Thumbnails extends React.Component {
   componentWillReceiveProps(nextProp) {
     this.setState({ restaurant: nextProp.restaurant });
   }
-  detectSizeHover = id => {
-    if (window.innerWidth >= 960) {
-      this.props.thumbnailHover(id);
-    }
-  };
-  detectSizeLeave = () => {
-    if (window.innerWidth >= 960) {
-      this.props.thumbnailLeave();
-    }
-  };
+
   render() {
     return (
       <Grid item xs={12} sm={6} lg={4}>
@@ -30,10 +21,10 @@ class Thumbnails extends React.Component {
           <div
             className="card"
             onMouseEnter={x => {
-              this.detectSizeHover(this.state.restaurant._id);
+              this.props.thumbnailHover(this.state.restaurant._id);
             }}
             onMouseLeave={x => {
-              this.detectSizeLeave();
+              this.props.thumbnailLeave();
             }}
           >
             <div

5fd88ca113a99ec34a06ac8550e564f9ffd3ea42
Jung Eun (Jessica) Kwak <jkwak39@gmail.com>
2020-11-05 16:33:50 +0800

    api key

--- src/components/restaurants.js
+++ src/components/restaurants.js
@@ -18,7 +18,7 @@ class Restaurants extends React.Component {
     selectedFeature: "",
     map: {
       key: {
-        key: "AIzaSyA2zALZWIPBhI-faRys04N0xBNwUxL3AT8"
+        key: "AIzaSyBKMVj4gaJLU9GTV1zOaWQj7ggKVbXQep0"
       },
       center: {
         lat: -8.655,
